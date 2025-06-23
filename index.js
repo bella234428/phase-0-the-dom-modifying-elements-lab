82b78528184ffb0a77eeb3379fd458fcbb12063a
bella234428 <ibseali258@gmail.com>
2025-06-23 20:09:18 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// Remove the 'main' element
+const main = document.getElementById("main");
+main.remove();
+
+// Create a new h1 element
+const newHeader = document.createElement("h1");
+
+// Set its id to 'victory'
+newHeader.id = "victory";
+
+// Set its text content
+newHeader.textContent = "Alexia is the champion";
+
+// Append it to the body (so it shows up in the DOM)
+document.body.append(newHeader);
